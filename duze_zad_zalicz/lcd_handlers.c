0a83f2e000338c9b491685716c7095409af1a1b8
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2025-01-19 17:38:32 +0100

    in lcd_handlers we now use modulo for negative cases

--- duze_zad_zalicz/lcd_handlers.c
+++ duze_zad_zalicz/lcd_handlers.c
@@ -16,13 +16,11 @@ void calc_new_lcd_pos(int8_t pos_change, int8_t line_change, QInfo *q_info)
         LCD_get_text_width_height(&text_font_width, &text_font_height);
     }
 
-
     // pos_change i line_change moga byc ujemne badz dodatnie wiec zawsze
     // najpierw dodajemy je do naszych zmiennych i potem odpowiednio 
     // modyfikujemy je zeby spelnialy warunki wymiarow LCD
     pos += pos_change;
     line += line_change;
-
 
     if (pos_change < 0)
     {
@@ -30,21 +28,13 @@ void calc_new_lcd_pos(int8_t pos_change, int8_t line_change, QInfo *q_info)
         // jesli pos < 0 to wyszlismy poza lewa krawedz LCD, 
         // Ale tylko jesli jestesmy dalej niz text_font_width od lewej krawedzi
         // to przechodzimy calkowicie na prawy koniec LCD.
-        // Wyraz (-pos - text_font_width) jest potrzebny dlatego, ze mozemy 
-        // przesunac sie nie o 1 w lewo, ale 2, 3, 4 itd. i wtedy powinnismy
-        // przeniesc nasz '+' jeszcze dalej od prawej krawedzi
+        // Robimy modulo bo mozemy sie przeniesc o wiecej niz 1 piksel w lewo 
+        // wiec poprzez modulo sprawdzamy ile razy przekroczylismy lewa krawedz
+        // i ile powinnismy byc od prawej krawedzi
         if (pos < 0 && -pos >= text_font_width)
         {
-            // while (-pos > text_font_width)
-            // {
-            // gdy -pos == text_font_width to po tej operacji bedzie 0 i nic nam
-            // sie nie zmieni w finalnej aktualizacji pos
-            // ale gdy -pos > text_font_width to bedziemy miec teraz w tym pos
-            // ten dodatkowy nadmiar ktory na koncu odejmiemy od finalnego pos
-            pos += text_font_width;
-            // }
-            
-            pos = LCD_WIDTH - text_font_width - (-pos);
+            pos = (-pos) % LCD_WIDTH;
+            pos = LCD_WIDTH - pos;
         }
     }
     else 
@@ -58,11 +48,8 @@ void calc_new_lcd_pos(int8_t pos_change, int8_t line_change, QInfo *q_info)
         // idziemy w gore
         if (line < 0 && -line >= text_font_height)
         {
-            // while (-line > text_font_height)
-            // {
-                line += text_font_height;
-            // }
-            line = LCD_HEIGHT - text_font_height - (-line);
+            line = (-line) % LCD_HEIGHT;
+            line = LCD_HEIGHT - line;
         }
     }
     else 
@@ -70,9 +57,6 @@ void calc_new_lcd_pos(int8_t pos_change, int8_t line_change, QInfo *q_info)
         // idziemy w dol
         line = line % LCD_HEIGHT;
     }
-
-    q_add_xyz_big(pos, 0, q_info);
-    q_add_xyz_big(line, 1, q_info);
 
     LCDclear();
     LCDgoto(line, pos);
